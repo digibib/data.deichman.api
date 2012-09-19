c806a9cd49b1d96317405f9305b3c0e390101763
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-09-19 14:37:34 +0200

    begins crafting the api

--- api.rb
+++ api.rb
@@ -1,4 +1,21 @@
+require "bundler/setup"
 require "grape"
+require "rdf/virtuoso"
+require "./vocabularies.rb"
+
+repository = YAML::load(File.open("config/repository.yml"))
+REPO = RDF::Virtuoso::Repository.new(
+              repository["sparql_endpoint"],
+              :update_uri => repository["sparul_endpoint"],
+              :username => repository["username"],
+              :password => repository["password"],
+              :auth_method => repository["auth_method"])
+QUERY  = RDF::Virtuoso::Query
+DEFAULT_GRAPH = RDF::URI('http://data.deichman.no/bookreviews')
+
+#sanitize_isbn: isbn.strip.gsub(/\s+|-/, '')
+isbn ="9788243006218"
+query = QUERY.select([])
 
 class API < Grape::API
   prefix 'api'
