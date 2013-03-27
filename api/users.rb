6749c46335ced9b3cd4bb0ed131813b2daffe721
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-03-27 07:36:51 +0100

    correct reponse status from 401 unathorized to 404 not found

--- api/users.rb
+++ api/users.rb
@@ -3,7 +3,7 @@ module API
   class Users < Grape::API
   # /api/users
     resource :users do
-  
+
       desc "returns all users or specific user"
       get "/" do
         content_type 'json'
@@ -18,12 +18,12 @@ module API
           {:reviewer => reviewer }
         end
       end
-  
+
       desc "creates a user"
         params do
           requires :api_key,     type: String, desc: "API key"
           requires :accountName, type: String, desc: "Reviewer's email", regexp: /[[:ascii:]]+@[[:ascii:]]+\.[[:ascii:]]{2,4}/
-        end    
+        end
       post "/" do
         content_type 'json'
         logger.info "params: #{params}"
@@ -33,7 +33,7 @@ module API
         reviewer.password = nil
         {:reviewer => reviewer}
       end
-      
+
       desc "updates a user"
         params do
           requires :api_key,   type: String, desc: "API key"
@@ -51,9 +51,9 @@ module API
         error!("Sorry, reviewer not found in our base", 400) unless reviewer
         reviewer.update(params)
         reviewer.password = nil
-        {:reviewer => reviewer}    
+        {:reviewer => reviewer}
       end
-      
+
       desc "deletes a user"
         params do
           requires :api_key,   type: String, desc: "API key"
@@ -66,9 +66,9 @@ module API
         error!("Sorry, \"#{params[:api_key]}\" is not a valid api key", 400) if reviewer == "Invalid api_key"
         error!("Sorry, \"#{params[:uri]}\" matches no review in our base", 400) unless reviewer
         result = reviewer.delete(params)
-        {:result => result}   
+        {:result => result}
       end
-      
+
       desc "authenticates a user"
         params do
           requires :username,   type: String, desc: "Reviewer accountName"
@@ -80,11 +80,11 @@ module API
         if user
           authenticated = true if user.accountName == params["username"] && user.authenticate(params["password"])
         else
-          error!("Sorry, username \"#{params[:username]}\" not found", 401)
+          error!("Sorry, username \"#{params[:username]}\" not found", 404)
         end
         status 200 if authenticated
         {:authenticated => authenticated}
-      end    
-    end 
+      end
+    end
   end
 end
